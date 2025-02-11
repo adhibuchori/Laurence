04042551241a3002399b3f687bcab4b31efc0906
Mochammad Adhi Buchori <adhi.buchori@gmail.com>
2025-02-11 15:50:31 +0700

    Configure tamagui

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import { createTamagui, TamaguiProvider } from 'tamagui';
+import {defaultConfig} from '@tamagui/config/v4';
+import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './src/navigation/RootStack';
 
-function App(){
+const config = createTamagui(defaultConfig);
+
+type Conf = typeof config;
+
+declare module '@tamagui/core' {
+  interface TamaguiCustomConfig extends Conf {}
+}
+
+function App() {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <TamaguiProvider config={config}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </TamaguiProvider>
   );
 }
 
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import styles from './styles';
+import { Button } from 'tamagui';
 
 function HomeScreen() {
   return (
     <View style={styles.container}>
+      <Button theme={'blue'}>Hello Home</Button>
       <Text>Home Screen</Text>
     </View>
   );
