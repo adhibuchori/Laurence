02d14245520982c647e1554a23386b078feb9d48
Mochammad Adhi Buchori <adhi.buchori@gmail.com>
2025-02-12 15:34:53 +0700

    Finalized splash screen feature

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import React, {useEffect} from 'react';
+import {StatusBar} from 'react-native';
 import styles from './styles';
-import { Text, View } from 'tamagui';
+import {Text, View} from 'tamagui';
 import AppLogo from '../../../assets/icons/laurence_app_logo.svg';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-function SplashScreen() {
+type RootStackParamList = {
+  Splash: undefined;
+  Onboarding: undefined;
+};
+
+type SplashScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'Onboarding'
+>;
+
+interface Props {
+  navigation: SplashScreenNavigationProp;
+}
+
+function SplashScreen({navigation}: Props) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.replace('Onboarding');
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
